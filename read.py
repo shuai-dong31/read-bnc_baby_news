e88bf79a1924471fc5b07ec8e3d77c458e135597
Wenyaun Wu <wenyuan.wu@uzh.ch>
2020-01-17 00:33:30 +0100

    fix xml parser error

--- read.py
+++ read.py
@@ -4,25 +4,25 @@ import os.path
 
 import fnmatch
 
+
 def get_content(filename):
 
-    content = open(filename,"w")
+    file_w = "{}.txt".format(filename)
+
+    content = open(file_w, "w")
 
     tree = ET.parse(filename)
 
     root = tree.getroot()
 
     for w in root.iter('w'):
-
         content.write(w.text)
 
-    filename.close()
 
 for file in os.listdir(os.getcwd()):
 
     if fnmatch.fnmatch(file, '*.xml'):
-
         get_content(file)
-        
-print('works are done,sucker')
-    
+
+
+print('run this line instead')
